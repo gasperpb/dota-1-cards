f2b77a1f83b3fb58314da78ae70893536ebb0896
gasperpb <achillespb@hotmail.com>
2022-11-27 17:37:01 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* Left */}
+
+            {/* Middle */}
+            {/* Right*/}
+
+
+
+        </div>
+    )
+}
+
+export default Navbar
